3e3d4bf337e43d3cf2b81d6014159a0122bf66be
Jordan S Read <jread@usgs.gov>
2015-04-12 21:45:45 -0500

    plotting functions take 1

---
+++ R/moisture_map.R
@@ -0,0 +1,52 @@
+#'@title create a leaflet map for soilmoisture
+#'@param depth in inches for sensors
+#'@param out_file html output file name for leaflet map
+#'@importFrom htmlwidgets saveWidget
+#'@importFrom dplyr select filter %>%
+#'@import leaflet
+#'@importFrom XML getNodeSet xmlRoot htmlParse
+#'@export
+moisture_map <- function(depth, out_file = 'soilmoisture_usgs.html'){
+  sites = scan_sites_by_element_code('SMS')
+  
+  ca_sites <- sites#[is_ca]
+  #Just use the default start/end date
+  data = scan_data(ca_sites, depth = depth, start = '2014-06-01', end = '2014-06-02')
+  soil_moisture <- select(data, station, value) %>% 
+    filter(!duplicated(station))
+  names(soil_moisture)[1] <- 'stationTriplet'
+  soil_moisture$stationTriplet <- as.character(soil_moisture$stationTriplet)
+  ca_metadata = scan_sites_metadata(soil_moisture$stationTriplet)
+  
+  
+  
+  
+  station_loc <- select(ca_metadata, name, latitude, longitude, stationTriplet)
+  station_loc <- left_join(station_loc, soil_moisture, by = 'stationTriplet')
+  
+  names(station_loc)[2] <- 'lat'
+  names(station_loc)[3] <- 'lng'
+  col_types <- c( "red","orange", "yellow", "grey80","dodgerblue","darkblue")
+  leg_vals <- pretty(station_loc$value)
+  cols <- colorNumeric(col_types, domain = leg_vals)
+  pops <- paste(station_loc$name, sprintf('<br/>(%1.1f)',station_loc$value))
+  
+  
+  
+  m = leaflet(station_loc) %>% 
+    addTiles(options = tileOptions(zIndex = 2, maxZoom = 8, minZoom = 4)) %>% 
+    addCircleMarkers(~lng, ~lat, popup = pops,
+                     fillColor = ~cols(value), color = "grey60", 
+                     fillOpacity = 0.8, radius = 6, opacity = 0.8, 
+                     options = markerOptions(zIndexOffset = -1)) %>%
+    setView(-109.36, 36.67, zoom = 4)
+  
+  saveWidget(m, 'soilmoisture_usgs.html', selfcontained = TRUE, libdir = NULL)
+  doc = htmlParse('soilmoisture_usgs.html',isURL=FALSE, useInternalNodes = TRUE)
+  top = xmlRoot(doc)
+  div_node <- getNodeSet(top, "//div[contains(@id,'htmlwidget_container')]/div[1]")
+  div_node[[1]] <- add_svg_legend(div_node[[1]],
+                                  cols = cols(leg_vals), names = leg_vals, title = 'Soil moisture [-]')
+  saveXML(doc, file = 'soilmoisture_usgs.html')
+  return(out_file)
+}
