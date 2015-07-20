e08625df8c4239f2ff0e567dc3ff637b55fd5117
Luke Winslow <lwinslow@usgs.gov>
2015-07-20 14:00:33 -0500

    add west texas mesonet

--- R/automated_web_update.R
+++ R/automated_web_update.R
@@ -8,8 +8,17 @@
 #'@export
 automated_web_update = function(outpath='.'){
 	
-	moisture_map(to_hist_percentile(ok_data()), 
-							 ok_sites_metadata(ok_data()$station), 
+	ok     = to_hist_percentile(ok_data())
+	ok_met = ok_sites_metadata(ok$station)
+	
+	tx     = to_hist_percentile(tx_data())
+	tx_met = tx_sites_metadata(tx$station)
+	
+	ok_met = ok_met[, intersect(names(ok_met), names(tx_met))]
+	tx_met = tx_met[, intersect(names(ok_met), names(tx_met))]
+	
+	moisture_map(rbind(tx, ok), 
+							 rbind(tx_met, ok_met),
 							 out_file= file.path(outpath, 'index.html'))
 	
 	
