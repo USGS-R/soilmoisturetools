9e0d5da4de303585b8317c8c7e69b984a39aed6e
Megan Hines <mhines@usgs.gov>
2017-04-27 16:17:35 -0500

    woo, got this fixed up.

--- R/scan_sites_by_element_code.R
+++ R/scan_sites_by_element_code.R
@@ -13,19 +13,18 @@
 #'@export
 scan_sites_by_element_code = function(elementCd){
 	
-	root = newXMLNode("soap:Envelope", namespaceDefinitions=c("soap"="http://schemas.xmlsoap.org/soap/envelope/", "xsd"="http://www.w3.org/2001/XMLSchema"))
+	root = newXMLNode("SOAP-ENV:Envelope", namespaceDefinitions=c("SOAP-ENV"="http://schemas.xmlsoap.org/soap/envelope/", "q0"="http://www.wcc.nrcs.usda.gov/ns/awdbWebService", "xsd"="http://www.w3.org/2001/XMLSchema", "xsi"="http://www.w3.org/2001/XMLSchema-instance"))
 	
-	body = newXMLNode("soap:Body", parent=root)
+	body = newXMLNode("SOAP-ENV:Body", parent=root)
 	
-	cmd = newXMLNode("tns:getStations", namespaceDefinitions=c("tns"="http://wcc.sc.egov.usda.gov/awdbWebService"), parent=body)
+	cmd = newXMLNode("q0:getStations", parent=body)
 	
 	newXMLNode("elementCds", elementCd, parent=cmd)
-	newXMLNode("ordinals", 1, parent=cmd)
 	newXMLNode("logicalAnd", "false", parent=cmd)
 	
 	out = POST(service_url, content_type("text/soap_xml; charset-utf-8"), body=toString.XMLNode(root))
-	sites = xpathSApply(content(out), '//return', xmlValue)
 	
+	sites = xpathSApply(content(out), '//return', xmlValue)
 	
 	#Currently filter and just return SCAN sites, this may change in the future
 	id_strings = sapply(sites, function(x){strsplit(x, ":")[[1]][3]})
