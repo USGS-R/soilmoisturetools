2d95e5a266cbad43da2e0f47b3e5a97b961d5ad1
Megan Hines <mhines@usgs.gov>
2017-04-28 09:54:53 -0500

    updating to only use 10 stationChunks at a time otherwise was encountering strange response behavior from the soap service

--- R/scan_sites_metadata.R
+++ R/scan_sites_metadata.R
@@ -16,32 +16,32 @@
 #'}
 #'@export
 scan_sites_metadata = function(stationTriplets){
-	
-	df_out = data.frame()
-	stationChunks = split(stationTriplets, ceiling(seq_along(stationTriplets)/1000))
-	
-	for(i in 1:length(stationChunks)){
-		
-		cmd = xmlNode("q0:getStationMetadataMultiple")
-		
-		stationNodes = lapply(stationChunks[[i]], xmlNode, name="stationTriplets")
-		cmd = addChildren(cmd, kids=stationNodes)
-			
-		body = xmlNode("SOAP-ENV:Body", cmd)
-		
-		root = xmlNode("SOAP-ENV:Envelope", namespaceDefinitions=c("SOAP-ENV"="http://schemas.xmlsoap.org/soap/envelope/", "q0"="http://www.wcc.nrcs.usda.gov/ns/awdbWebService", "xsd"="http://www.w3.org/2001/XMLSchema", "xsi"="http://www.w3.org/2001/XMLSchema-instance"), body)
-		
-		out = POST(service_url, content_type("text/soap_xml; charset-utf-8"), body=toString.XMLNode(root))
-		#values = xpathSApply(content(out), '//values', xmlValue)
-		
-	
-		df_out = rbind(df_out, xmlToDataFrame(nodes=xpathApply(content(out), '/*/*/*/return')))
-	}
-	
-	df_out$elevation = as.numeric(df_out$elevation)
-	df_out$latitude  = as.numeric(df_out$latitude)
-	df_out$longitude = as.numeric(df_out$longitude)
-	df_out$station   = df_out$name
-	
-	return(df_out)
+  
+  df_out = data.frame()
+  stationChunks = split(stationTriplets, ceiling(seq_along(stationTriplets)/10))
+  
+  for(i in 1:length(stationChunks)){
+    
+    cmd = xmlNode("q0:getStationMetadataMultiple")
+    
+    stationNodes = lapply(stationChunks[[i]], xmlNode, name="stationTriplets")
+    cmd = addChildren(cmd, kids=stationNodes)
+    
+    body = xmlNode("SOAP-ENV:Body", cmd)
+    
+    root = xmlNode("SOAP-ENV:Envelope", namespaceDefinitions=c("SOAP-ENV"="http://schemas.xmlsoap.org/soap/envelope/", "q0"="http://www.wcc.nrcs.usda.gov/ns/awdbWebService", "xsd"="http://www.w3.org/2001/XMLSchema", "xsi"="http://www.w3.org/2001/XMLSchema-instance"), body)
+    
+    out = POST(service_url, content_type("text/soap_xml; charset-utf-8"), body=toString.XMLNode(root))
+    #values = xpathSApply(content(out), '//values', xmlValue)
+    
+    
+    df_out = rbind(df_out, xmlToDataFrame(nodes=xpathApply(content(out), '/*/*/*/return')))
+  }
+  
+  df_out$elevation = as.numeric(df_out$elevation)
+  df_out$latitude  = as.numeric(df_out$latitude)
+  df_out$longitude = as.numeric(df_out$longitude)
+  df_out$station   = df_out$name
+  
+  return(df_out)
 }
